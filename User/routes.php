fdd858ff76e170fde17c4ca4143bb99e9661b247
Huynh van luu <vanluujonny@gmail.com>
2019-06-04 13:32:21 +0700

    up1

--- User/routes.php
+++ User/routes.php
@@ -1,12 +1,7 @@
 <?php
 
-Route::group(['middleware' => ['web','App\Http\Middleware\Users'], 'module' => 'User', 'prefix' => 'users', 'namespace' => 'Ext\User\Controllers'], function() {
-  	Route::get('/mail_alert', 'UserAdminController@mail_alert')->name('mail_alert');
-	Route::post('/mail_alert', 'UserAdminController@post_mail_alert')->name('post_mail_alert');
-	Route::get('/bookmark', 'UserAdminController@bookmark')->name('bookmark');
-	Route::post('/ajaxbookmark', 'UserAdminController@ajaxbookmark')->name('ajaxbookmark');
-
-	Route::get('/edituser', 'UserController@edituser')->name('edituser');
-	Route::post('/edituser', 'UserController@postedituser')->name('postedituser');
-  	
+Route::group(['middleware' => ['web'], 'module' => 'User', 'prefix' => 'user', 'namespace' => 'App\Modules\User\Controllers'], function() {
+  	Route::get('/', function(){
+        return 'hello user';
+    })->name('user');
 });
